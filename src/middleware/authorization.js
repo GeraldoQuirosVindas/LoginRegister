60dc9434f38ce30925236a151b8a5b66c8371a73
Geraldo QV <gequirosv@gmail.com>
2024-03-25 16:14:01 -0600

    commit

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -2,8 +2,6 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import userModel from "../models/userModel.js";
 
-import { Messages } from "../utils/constans.js";
-
 dotenv.config();
 
 function soloAdmin(req, res, next) {
@@ -14,21 +12,19 @@ function soloAdmin(req, res, next) {
 
 function soloPublic(req, res, next) {
     const logged = checkingCookie(req);
-    console.log(logged)
     if (!logged) return next();
     return res.redirect("/admin")
 };
 
 
-async function checkingCookie(req) {
+function checkingCookie(req) {
     try {
-        //console.log("COOKIE",req.headers.cookie)
+        //console.log("COOKIE", req.headers.cookie)
         const cookieJWT = req.headers.cookie.split(";").find(cookie => cookie.startsWith("jwt=")).slice(4);
-        //console.log("COOKIE SLICE",cookieJWT)
-        const decrypt = jwt.verify(cookieJWT, process.env.JWT_SECRET);
-        console.log(decrypt)
-        const user = await userModel.findOne({ email: emailDB });
-        console.log("x", user)
+        //console.log("COOKIE SLICE", cookieJWT)
+        //const decrypt = jwt.verify(cookieJWT, process.env.JWT_SECRET);
+        //console.log(decrypt)
+        const user = userModel.findOne({ email: req.headers.email });
         if (!user) {
             return false;
         }
